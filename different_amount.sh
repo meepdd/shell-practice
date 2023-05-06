c58ecbfba7c9b253c701bff1b2d43faeb76e2928
MD <101170461+meepdd@users.noreply.github.com>
2023-05-06 23:03:31 +0300

    Test №1.3
    Checking that the script outputs an error message when transferring files with a different number of lines

---
+++ different_amount.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Создаем два файла с группами студентов
+echo "Ivanov
+Petrov
+Sidorov" > group1.txt
+
+echo "Smith
+Johnson" > group2.txt
+
+# Ожидаемый вывод: "Количество людей в группах не совпадает" и "NO"
