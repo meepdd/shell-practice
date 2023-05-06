ac3157317570862e3dab3869dfb54d11fddd46fc
MD <101170461+meepdd@users.noreply.github.com>
2023-05-06 22:58:40 +0300

    Test №1.1
    Checking that the script works correctly when two files with the same number of lines are transferred

---
+++ equal_amount.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Создаем два файла с группами студентов
+echo "Ivanov
+Petrov
+Sidorov" > group1.txt
+
+echo "Smith
+Johnson
+Williams" > group2.txt
+
+# Запускаем скрипт
+./script.sh group1.txt group2.txt
+
+# Ожидаемый вывод: "В обоих группах 3 человек"
